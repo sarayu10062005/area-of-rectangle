3367d84b1b4c324a6165ca1bee756f2dbc2766c2
sarayu10062005 <srijasarayu@gmail.com>
2025-01-26 02:53:45 -0800

    Add files via upload

---
+++ AreaOR.java
@@ -0,0 +1,11 @@
+public class AreaOR {
+    public static void main(String[] args) {
+        int Len = 12;
+        float Wid = 4.0f;
+        float Area = Len * Wid;
+
+        System.out.printf("Area of Rectangle: Length times Width = %.2f", Area);
+        System.out.println("\nIn terms of integers: " + (int) Area);
+
+    }
+}
